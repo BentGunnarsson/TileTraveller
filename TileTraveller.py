9a52ac7c786fcbb11179c61e5cde4648217bf7dc
Bent Gunnarsson <bent19@ru.is>
2019-10-08 10:41:02 +0000

    Random choice kind of added

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,8 +1,19 @@
 #https://github.com/BentGunnarsson/TileTraveller
-
+import random
 # User starts in (1,1) with 0 coins
 x, y = 1, 1
 coin_count = 0
+valid_moves = 0
+def random_move():
+    direction_list = ["n","e","s","w"]
+    direction = random.choice(direction_list)
+    print("Direction:", direction)
+    return direction
+
+def random_answer():
+    yes_or_no_list = ["y","n"]
+    answer = random.choice(yes_or_no_list)
+    return answer
 
 def displacement(user_input):
     """
@@ -19,11 +30,16 @@ def displacement(user_input):
         return x-1, y-0
 
 def pull_lever(coins):
-    answer = input("Pull a lever (y/n): ")
+    #answer = input("Pull a lever (y/n): ")
+    answer = random_answer()
+    print("Pull a lever (y/n):",answer)
     if answer == "y":
         coins += 1
         print("You received 1 coin, your total is now "+str(coins)+".")
     return coins
+
+seed = input("Input seed: ")
+random.seed(a=seed, version=2)
 
 #While loop runs until we win
 while True:
@@ -31,8 +47,10 @@ while True:
         
         while True:
             print("You can travel: (N)orth.")
-            direction = input('Direction: ').lower()
+            #direction = input('Direction: ').lower()
+            direction = random_move()
             if direction == 'n':
+                valid_moves += 1
                 break
             else:
                 print('Not a valid direction!')
@@ -42,8 +60,10 @@ while True:
         coin_count = pull_lever(coin_count)
         while True:
             print("You can travel: (N)orth or (E)ast or (S)outh.")
-            direction = input('Direction: ').lower()
+            #direction = input('Direction: ').lower()
+            direction = random_move()
             if direction == 'e' or direction == 'n' or direction == 's':
+                valid_moves += 1
                 break
             else:
                 print('Not a valid direction!')
@@ -52,8 +72,11 @@ while True:
     elif x == 1 and y == 3:
         while True:
             print("You can travel: (E)ast or (S)outh.")
-            direction = input('Direction: ').lower()
+            #direction = input('Direction: ').lower()
+            direction = random_move()
+            print("Direction:", direction)
             if direction == 'e' or direction == 's':
+                valid_moves += 1
                 break
             else:
                 print('Not a valid direction!')
@@ -63,8 +86,10 @@ while True:
         coin_count = pull_lever(coin_count)
         while True:
             print("You can travel: (S)outh or (W)est.")
-            direction = input('Direction: ').lower()
+            #direction = input('Direction: ').lower()
+            direction = random_move()
             if direction == 'w' or direction == 's':
+                valid_moves += 1
                 break
             else:
                 print('Not a valid direction!')
@@ -74,8 +99,10 @@ while True:
         coin_count = pull_lever(coin_count)
         while True:
             print("You can travel: (E)ast or (W)est.")
-            direction = input('Direction: ').lower()
+            #direction = input('Direction: ').lower()
+            direction = random_move()
             if direction == 'e' or direction == 'w':
+                valid_moves += 1
                 break
             else:
                 print('Not a valid direction!')
@@ -85,8 +112,10 @@ while True:
         coin_count = pull_lever(coin_count)
         while True:
             print("You can travel: (N)orth or (S)outh.")
-            direction = input('Direction: ').lower()
-            if direction == 'e' or direction == 'w' or direction == 'n' or direction == 's':
+            #direction = input('Direction: ').lower()
+            direction = random_move()
+            if direction == 'n' or direction == 's':
+                valid_moves += 1
                 break
             else:
                 print('Not a valid direction!')
@@ -95,18 +124,21 @@ while True:
     elif x == 3 and y == 3:
         while True:
             print("You can travel: (S)outh or (W)est.")
-            direction = input('Direction: ').lower()
+            #direction = input('Direction: ').lower()
+            direction = random_move()
             if direction == 'w' or direction == 's':
+                valid_moves += 1
                 break
             else:
                 print('Not a valid direction!')
         x, y = displacement(direction)
     # (3,1)       VICTORY so we break
     elif x == 3 and y == 1:
-        print("Victory! Total coins "+str(coin_count)+".")
-        #again = input("Play again (y/n): ")
-        #if again == "y":
-        #    coin_count = 0
-        #    x ,y = 1, 1
-        #else:
-        break
+        print("Victory! Total coins "+str(coin_count)+". Valid moves",valid_moves)
+        again = input("Play again (y/n): ")
+        if again == "y":
+            coin_count = 0
+            valid_moves = 0
+            x ,y = 1, 1
+        else:
+            break
